a7bce962e043478dea47aa8b48d06869b59e9eff
Nguyen Vinh Linh <nguyenvinhlinh@localhost.localdomain>
2019-04-07 13:22:40 +0700

    add music alias

--- main.zsh
+++ main.zsh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 source $MY_ALIAS/dnf.sh;
 source $MY_ALIAS/system.sh;
+source $MY_ALIAS/music.sh;
---
+++ music.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+alias beautiful_life="mpv --no-video $HOME/Music/Beautiful_Life"
+alias beethoven="mpv --no-video $HOME/Music/Beethoven"
+alias chat_voi_mozart2="mpv --no-video $HOME/Music/Chat_Với_Mozart_vol.2"
+alias ru_rung="mpv --no-video $HOME/Music/Ru_Rừng"
+alias street_rhythm="mpv --no-video $HOME/Music/Street_Rhythm"
