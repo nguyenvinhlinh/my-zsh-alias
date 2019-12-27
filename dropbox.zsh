d16a481c0cd32f5e0c9dfaed325443193bfd63d2
Nguyễn Vĩnh Linh <nguyenvinhlinh93@gmail.com>
2019-12-27 10:21:38 +0700

    add dropbox + phoenix.

---
+++ dropbox.zsh
@@ -0,0 +1,3 @@
+#!/usr/bin/env zsh
+
+alias dropboxr="dropbox stop; dropbox start;"
--- main.zsh
+++ main.zsh
@@ -3,3 +3,5 @@
 source $MY_ALIAS/dnf.zsh;
 source $MY_ALIAS/system.zsh;
 source $MY_ALIAS/music.zsh;
+source $MY_ALIAS/phoenix.zsh;
+source $MY_ALIAS/dropbox.zsh;
