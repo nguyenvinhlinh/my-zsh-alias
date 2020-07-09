c61a9839a909599ca5423bbcec9d28d20f368c7c
Nguyễn Vĩnh Linh <nguyenvinhlinh93@gmail.com>
2020-07-09 14:25:30 +0700

    add util to download youtbe audio

--- music.zsh
+++ music.zsh
@@ -6,3 +6,6 @@ alias chat_voi_mozart2="mpv --no-video $HOME/Music/Chat_Với_Mozart_vol.2"
 alias ru_rung="mpv --no-video $HOME/Music/Ru_Rừng"
 alias street_rhythm="mpv --no-video $HOME/Music/Street_Rhythm"
 alias chillout="mpv --no-video --shuffle $HOME/Music/Chillout"
+alias jazz="mpv --no-video --shuffle $HOME/Music/Jazz/"
+
+alias youd="youtube-dl --write-thumbnail --extract-audio --audio-format flac --audio-quality 0"
