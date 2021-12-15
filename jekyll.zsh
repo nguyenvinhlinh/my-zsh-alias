8cb8b506e45694d7f09f4f3b8c51ec9255c758d0
Nguyen Vinh Linh <nguyenvinhlinh93@gmail.com>
2021-12-16 01:10:59 +0700

    update jekyll post generator alias

--- jekyll.zsh
+++ jekyll.zsh
@@ -1,2 +1,2 @@
-alias jekyll_post="java -jar /home/nguyenvinhlinh/Software/JekyllPostGenerator/JekyllPostGenerate.jar"
+alias jekyll_post="java -jar /home/nguyenvinhlinh/Software/JekyllPostGenerator/jekyll-post-generator-2.0.0.jar"
 alias jekyll_ser="rm -rf _site; bundle exec jekyll server --watch;"
