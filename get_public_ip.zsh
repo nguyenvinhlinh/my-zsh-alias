e97ad0593a50a749d057b6a74680724f412942e4
Nguyen Vinh Linh <nguyenvinhlinh93@gmail.com>
2025-10-16 03:30:50 +0700

    update

--- get_public_ip.zsh
+++ get_public_ip.zsh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 function get_public_ip() {
     public_ip=$(curl -s https://api.ipify.org)
-    echo "IP: $public_ip (copied to clipboard)"
     wl-copy "$public_ip"
+    echo "IP: $public_ip"
 }
